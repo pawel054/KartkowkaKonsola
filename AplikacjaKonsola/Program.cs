ef5f1b75791025b48a0632e937025ab887a0d370
pawel054 <pawel.marcisz1@gmail.com>
2024-10-23 08:54:56 +0200

    Add LoadDataFromFile function

--- AplikacjaKonsola/Program.cs
+++ AplikacjaKonsola/Program.cs
@@ -2,9 +2,37 @@
 {
     internal class Program
     {
+        static List<AlbumData> albumDatas = new List<AlbumData>();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string filePath = System.IO.Path.Combine(Directory.GetCurrentDirectory(), "Data.txt");
+            LoadDataFromFile(filePath);
+        }
+
+        internal static void LoadDataFromFile(string filePath)
+        {
+            if(File.Exists(filePath))
+            {
+                List<string> fileLines = new List<string>();
+                var lines = File.ReadAllLines(filePath);
+                foreach(var line in lines)
+                {
+                    fileLines.Add(line);
+                }
+
+                for(int i = 0; i < fileLines.Count; i+=6)
+                {
+                    AlbumData albumData = new AlbumData
+                    {
+                        Artist = fileLines[i],
+                        Album = fileLines[i + 1],
+                        SongsNumber = int.Parse(fileLines[i + 2]),
+                        Year = int.Parse(fileLines[i + 3]),
+                        DownloadNumber = int.Parse(fileLines[i + 4])
+                    };
+                    albumDatas.Add(albumData);
+                }
+            }
         }
     }
 }
