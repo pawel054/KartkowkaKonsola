9baf9dfa1123a13567824d75bfaea0d086be7523
pawel054 <pawel.marcisz1@gmail.com>
2024-10-23 08:39:06 +0200

    Add AlbumData Class

---
+++ AplikacjaKonsola/AlbumData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplikacjaKonsola
+{
+    public class AlbumData
+    {
+        public string Artist { get; set; }
+        public string Album { get; set; }
+        public int SongsNumber {  get; set; }
+        public int Year {  get; set; }
+        public int DownloadNumber {  get; set; }
+    }
+}
